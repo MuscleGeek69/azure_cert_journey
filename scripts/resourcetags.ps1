605d0e60c5ce22f23d1bc16b2f546bf9eaaec22a
MuscleGeek69 <yanieralvarez@hotmail.com>
2023-03-24 17:49:15 -0400

    Prompt for VM name as well as SSH key

---
+++ scripts/resourcetags.ps1
@@ -0,0 +1,36 @@
+$taggableArray = "$(taggable)".Split(',')
+
+foreach ($resource in
+    ((az resource list --query "[?tags.CreatedOnDate == null]") | ConvertFrom-Json | Where { $taggableArray -contains $_.Type }))
+    {
+       if($resource.CreatedTime -eq $null) {
+            Write-Host -Message "--- CreatedTime is not available on $( $resource.Name ) --- "
+            continue
+        }
+
+        $resourceTags = ($resource.Tags | ConvertTo-Json | ConvertFrom-Json -AsHashTable) ?? @{}
+
+        $createdTime = [Datetime]::ParseExact($resource.CreatedTime,'MM/dd/yyyy HH:mm:ss',$null).ToString('MM/dd/yyyy')
+
+        $resourceTags += @{CreatedOnDate=$createdTime}
+
+        Set-AzResource -Tag $resourceTags -ResourceId $resource.Id -Force -ErrorAction continue
+        Write-Host  "Resource $( $resource.Name ) has had tags applied"
+        Write-Host  ' '
+}
+
+
+
+
+
+$resource = ((az resource list -n "$(ResourceName)") | ConvertFrom-Json)[0]
+
+$resourceTags = ($resource.Tags | ConvertTo-Json | ConvertFrom-Json -AsHashTable) ?? @{}
+
+$resourceTags += @{"$(TagName)"="$(TagValue)"}
+
+
+Set-AzResource -Tag $resourceTags -ResourceId $resource.Id -Force -ErrorAction continue
+Write-Host  "Resource $( $resource.Name ) has had tags applied"
+
+Set-AzRedisCache -Name "CacheName" -ResourceGroupName "ResourceGroupName" -RedisVersion "6"
