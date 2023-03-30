8a15983bbee6a262992fceff12504486e20ae770
MuscleGeek69 <yanieralvarez@hotmail.com>
2023-03-30 10:46:33 -0400

    password change

--- GitHubARMTemplateDeplotment.ps1
+++ GitHubARMTemplateDeplotment.ps1
@@ -9,6 +9,6 @@ New-AzResourceGroup -name $RGN -Location $Location
 
 $grg = (Get-AzResourceGroup).ResourceGroupName
 
-New-AzResourceGroupDeployment -name deployment1 -ResourceGroup $grg -TemplateUri "https://raw.githubusercontent.com/MuscleGeek69/az-104/main/az104/vm_arm_template.json"
+New-AzResourceGroupDeployment -name deployment5 -ResourceGroup $grg -TemplateUri "https://raw.githubusercontent.com/MuscleGeek69/az-104/main/az104/vm_arm_template.json"
 
 start-azvm -Name frontendSERVER -ResourceGroupName $RGN 
